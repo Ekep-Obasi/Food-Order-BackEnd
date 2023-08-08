0f2a629fb11e1753e8b5986d97bba816994d8ef8
unknown <arreyettaekep@gmail.com>
2023-08-08 15:32:46 +0100

    Updated the routes
    - Moved the GetVendors, GetVendorByID and CreateVendor routes to the
      AdminController
    - Created the Login, GetVendorProfile, UpdateVendorProfile and and
      UpdateVendorService that make use of the authentication middleware

--- controller/VendorController.ts
+++ controller/VendorController.ts
@@ -1,50 +1,95 @@
 import { Request, Response, NextFunction } from "express";
+import { EditVendorInputs, LoginVendorInputs } from "../dto";
+import { FindVendor } from "./AdminController";
+import { GenerateToken, ValidatePassword } from "../utility";
 import { Vendor } from "../models";
-import { GenerateSalt, GeneratedHashedPassword } from "../utility";
-import { CreateVendorInputs } from "../dto";
 
-export const CreateVendor = async ( req: Request, res: Response, next: NextFunction ) => {
+
+export const VendorLogin = async (req:Request, res:Response, next:NextFunction) => {
   
-  const { name, ownerName, foodType, pincode, address, phone, email, password } = <CreateVendorInputs>req.body;
+  const { email, password } = <LoginVendorInputs>req.body;
 
-  const existingVendor = await Vendor.findOne({ email }); // Verifying if a user already exists to avoid duplicates
+  const existingVendor = await FindVendor('', email); // Verify if there exitst a vendor with such email
 
-  if (existingVendor !== null)
-    return res.json({ message: "Already extist in the db" });
+  if(existingVendor !== null ){
 
+    const validation = await ValidatePassword(password, existingVendor.password, existingVendor.salt);
 
-    // generate salt & hash password
+    if(validation) {
 
-  const saltRounds = await GenerateSalt();
-  const hashedPassword = await GeneratedHashedPassword(password, saltRounds);
+      const token = await GenerateToken({ 
+        _id: existingVendor.id,
+        name: existingVendor.name, 
+        email: existingVendor.email,
+        foodType: existingVendor.foodType,
+       })
 
-  const createdVendor = await Vendor.create({
-    name,
-    ownerName,
-    foodType,
-    pincode,
-    address,
-    phone,
-    email,
-    password: hashedPassword,
-    salt: saltRounds,
-    serviceAvailalble: false,
-    rating: 0,
-    coverImage: [],
-    food: [],
-  });
+      return res.json(token);
+    }
+    else {
+      return res.json({ message: "Password is not valid" });
+    }
+  }
 
-  return res.json(createdVendor);
-};
+  res.json({ message: "Login credentials not valid"});
+}
 
-export const GetVendors = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {};
+export const GetVendorProfile = async (req:Request, res:Response, next:NextFunction) => {
+  
+  const user = req.user;
 
-export const GetVendorByID = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {};
+    if(user) {
+      const existingVendor = await FindVendor(user._id);
+
+      return  res.json(existingVendor);
+    }
+
+    return res.json({ message: "Vendor information not found"});  
+}
+
+export const UpdateVendorProfile = async (req:Request, res:Response, next:NextFunction) => {
+
+  const {name, phone, address, foodType} = <EditVendorInputs>req.body;
+
+  const user = req.user;
+
+    if(user) {
+      const existingVendor = await FindVendor(user._id);
+
+      if(existingVendor !== null) {
+        existingVendor.name = name;
+        existingVendor.phone = phone;
+        existingVendor.address = address;
+        existingVendor.foodType = foodType;
+
+        const updatedVendorProfile = await existingVendor.save();
+        res.json(updatedVendorProfile)
+      }
+
+      return  res.json(existingVendor);
+    }
+
+    return res.json({ message: "Vendor information not found"});
+}
+
+export const UpdateVendorService = async (req:Request, res:Response, next:NextFunction) => {
+  
+  const user = req.user;
+
+    if(user) {
+      const existingVendor = await FindVendor(user._id);
+
+      if(existingVendor !== null) {
+
+        existingVendor.serviceAvailalble = !existingVendor.serviceAvailalble;
+
+        const updatedVendorService = await existingVendor.save();
+
+        return res.json(updatedVendorService);
+      }
+
+      return  res.json(existingVendor);
+    }
+
+    return res.json({ message: "Vendor information not found"});
+}
