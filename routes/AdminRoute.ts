9fb9f8084e1654ad90a6703e39affd498428b53d
unknown <arreyettaekep@gmail.com>
2023-08-08 15:46:03 +0100

    Added the controllers to their respective routes
    - POST /vendor --> CreateVendor
    - GET /vendor/:id --> GetVendorByID
    - GET /vendor --> GetVendors

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
+import { CreateVendor, GetVendorByID, GetVendors } from "../controller";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello from Admin");
 });
+
+router.get('/vendor', GetVendors); // get all vendors
+router.get('/vendor/:id', GetVendorByID); // getVendor by id
+router.post('/vendor', CreateVendor); // Create vendor
 
 export { router as adminRouter };
